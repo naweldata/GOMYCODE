d51775ca591ad398130666168152e33317bf82e2
naweldata <ammarkhodja.nawel@gmail.com>
2023-03-04 22:50:33 +0100

    update

---
+++ MachineLearningSupervised/Regression/HousePrediction/HousePrediction.py
@@ -0,0 +1,10 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.feature_selection import VarianceThreshold
+
+#Recuperer la Data
+df=pd.read_csv("C:\\Users\\USER\\Documents\\GOMYCODE\\MachineLearningSupervised\\Regression\\HousePrediction\\Housing.csv", sep=';')
+df.head
+
